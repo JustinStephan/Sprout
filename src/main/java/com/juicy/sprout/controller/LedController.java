8102a3b55115d8279994bd0dbb24d0f44613645a
jay_s <jay_stefan@yahoo.com>
2022-03-05 17:43:40 -0500

    Changed GPIO pin number

--- src/main/java/com/juicy/sprout/controller/LedController.java
+++ src/main/java/com/juicy/sprout/controller/LedController.java
@@ -4,17 +4,21 @@ import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 
 @RestController
 public class LedController {
+//    private final static Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     private static GpioPinDigitalOutput pin;
 
     @RequestMapping("/led")
     public String toggleLed(){
+
         if (pin == null) {
             GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26,"MyLED", PinState.LOW);
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25,"MyLED", PinState.LOW);
         }
 
         pin.toggle();
