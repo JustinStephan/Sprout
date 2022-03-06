7cb870a0a889f55b614faeb59c9bf26f3b3c620b
jay_s <jay_stefan@yahoo.com>
2022-03-06 00:02:19 -0500

    Debugging

--- PythonScripts/dht2.py
+++ PythonScripts/dht2.py
@@ -1,11 +1,21 @@
-#Libraries
+# Libraries
 import Adafruit_DHT as dht
 from time import sleep
-#Set DATA pin
+
+# Set DATA pin
 DHT = 4
-while True:
-    #Read Temp and Hum from DHT22
-    h,t = dht.read_retry(dht.DHT22, DHT)
-    #Print Temperature and Humidity on Shell window
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
-    sleep(5) #Wait 5 seconds and read again
+
+
+def getTempAndHumid():
+	h, t = dht.read_retry(dht.DHT22, DHT)
+
+	return h, t
+
+
+def alwaysRead():
+	while True:
+		# Read Temp and Hum from DHT22
+		h, t = getTempAndHumid()
+		# Print Temperature and Humidity on Shell window
+		print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+		sleep(5)  # Wait 5 seconds and read again
--- src/main/java/com/juicy/sprout/controller/TempHumidityController.java
+++ src/main/java/com/juicy/sprout/controller/TempHumidityController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 @RestController
@@ -33,6 +34,42 @@ public class TempHumidityController {
         p.waitFor();
         System.out.println("Temperature is : " + temperature + " 'C Humidity is :" + humidity + " %RH");
         System.out.println("Done.");
+    }
 
+    @RequestMapping("/temp2")
+    public void getTempHumid() {
+        String s = null;
+
+        try {
+
+            // run the Unix "ps -ef" command
+            // using the Runtime exec method:
+            Process p = Runtime.getRuntime().exec("python3 ~/projects/Sprout/PythonScripts/dht2.py");
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            System.exit(0);
+        }
+        catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
