fa709ee7516e9f77d22cd9df184777c6a3e3dfc1
jay_s <jay_stefan@yahoo.com>
2022-03-05 23:41:24 -0500

    Debugging

--- PythonScripts/dht.py
+++ PythonScripts/dht.py
@@ -38,12 +38,12 @@ try:
 		while data[count] == 0:
 			tmp = 1
 			count = count + 1
-        print("test2")
+			print("test2")
 
 		while data[count] == 1:
 			bit_count = bit_count + 1
 			count = count + 1
-        print("test3")
+			print("test3")
 
 		if bit_count > 3:
 			if i>=0 and i<8:
