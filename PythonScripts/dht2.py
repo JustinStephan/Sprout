015f2f3dcf8023bfdd354bf64d3ce00414b24230
jay_s <jay_stefan@yahoo.com>
2022-03-06 00:12:38 -0500

    debugging

--- PythonScripts/dht2.py
+++ PythonScripts/dht2.py
@@ -5,20 +5,17 @@ from time import sleep
 # Set DATA pin
 DHT = 4
 
-
-def getTempAndHumid():
+#
+# def getTempAndHumid():
+# 	h, t = dht.read_retry(dht.DHT22, DHT)
+#
+# 	return h, t
+#
+#
+# def alwaysRead():
+while True:
+	# Read Temp and Hum from DHT22
 	h, t = dht.read_retry(dht.DHT22, DHT)
-
-	return h, t
-
-
-def alwaysRead():
-	while True:
-		# Read Temp and Hum from DHT22
-		h, t = getTempAndHumid()
-		# Print Temperature and Humidity on Shell window
-		print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
-		sleep(5)  # Wait 5 seconds and read again
-
-if __name__ == "__main__":
-	getTempAndHumid()
+	# Print Temperature and Humidity on Shell window
+	print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+	sleep(5)  # Wait 5 seconds and read again
