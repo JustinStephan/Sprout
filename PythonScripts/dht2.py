96159c73125c556b0bca847451415c35d23f0600
jay_s <jay_stefan@yahoo.com>
2022-03-06 12:44:29 -0500

    removed while loop in the python script so it only read and prints once

--- PythonScripts/dht2.py
+++ PythonScripts/dht2.py
@@ -13,9 +13,9 @@ pin = 19
 #
 #
 # def alwaysRead():
-while True:
-	# Read Temp and Hum from DHT22
-	h, t = dht.read_retry(dht.DHT22, pin)
-	# Print Temperature and Humidity on Shell window
-	print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
-	sleep(5)  # Wait 5 seconds and read again
+
+# Read Temp and Hum from DHT22
+h, t = dht.read_retry(dht.DHT22, pin)
+# Print Temperature and Humidity on Shell window
+print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+
