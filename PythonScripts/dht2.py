dc3a95dbd87b268c50715ed187509efdb1708186
jay_s <jay_stefan@yahoo.com>
2022-03-06 12:12:28 -0500

    Debugging

--- PythonScripts/dht2.py
+++ PythonScripts/dht2.py
@@ -3,11 +3,11 @@ import Adafruit_DHT as dht
 from time import sleep
 
 # Set DATA pin
-DHT = 19
+pin = 19
 
 #
 # def getTempAndHumid():
-# 	h, t = dht.read_retry(dht.DHT22, DHT)
+# 	h, t = dht.read_retry(dht.DHT22, pin)
 #
 # 	return h, t
 #
@@ -15,7 +15,7 @@ DHT = 19
 # def alwaysRead():
 while True:
 	# Read Temp and Hum from DHT22
-	h, t = dht.read_retry(dht.DHT22, DHT)
+	h, t = dht.read_retry(dht.DHT22, pin)
 	# Print Temperature and Humidity on Shell window
 	print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
 	sleep(5)  # Wait 5 seconds and read again
