10f213b1599abc40e6517639a6de25dba9c99916
jay_s <jay_stefan@yahoo.com>
2022-03-06 23:46:03 -0500

    Changed /temp to a simple print to prep for just a temp return

--- src/main/java/com/juicy/sprout/controller/TempHumidityController.java
+++ src/main/java/com/juicy/sprout/controller/TempHumidityController.java
@@ -17,23 +17,24 @@ public class TempHumidityController {
     @RequestMapping("/temp")
     public void getTemp() throws Exception{
         Runtime rt = Runtime.getRuntime();
-        Process p = rt.exec("python PythonScripts/dht2.py");
-        BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        if ((line = bri.readLine()) != null) {
+        Process p = rt.exec("python PyScripts/dht2.py");
+        BufferedReader br = new BufferedReader(new
+                InputStreamReader(p.getInputStream()));
+        if ((line = br.readLine()) != null) {
             if (!(line.contains("ERR_CRC") || line.contains("ERR_RNG"))) {
-
-                data = line.split("ABC");
-                System.out.println(data[0]);
-                temperature = Integer.parseInt(data[0]);
-                humidity = Integer.parseInt(data[1]);
+                System.out.println(line);
+//                data = line.split("ABC");
+//                System.out.println(data[0]);
+//                temperature = Integer.parseInt(data[0]);
+//                humidity = Integer.parseInt(data[1]);
             } else
                 System.out.println("Data Error");
         }
 
-        bri.close();
+        br.close();
         p.waitFor();
-        System.out.println("Temperature is : " + temperature + " 'C Humidity is :" + humidity + " %RH");
-        System.out.println("Done.");
+//        System.out.println("Temperature is : " + temperature + " 'C Humidity is :" + humidity + " %RH");
+//        System.out.println("Done.");
     }
 
     @RequestMapping("/temp2")
