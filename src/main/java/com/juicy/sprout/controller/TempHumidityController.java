112f7c396c0fae24b44bad02d0e341a43583c49a
jay_s <jay_stefan@yahoo.com>
2022-03-07 00:10:58 -0500

    Added a little extra flavor to the sensor gets

--- src/main/java/com/juicy/sprout/controller/TempHumidityController.java
+++ src/main/java/com/juicy/sprout/controller/TempHumidityController.java
@@ -32,12 +32,12 @@ public class TempHumidityController {
 
     @RequestMapping("/sensor/temp")
     public String getTemp() throws Exception{
-        return getReading("temp") + " 'C";
+        return "TEMP: " + getReading("temp") + " 'C";
     }
 
     @RequestMapping("/sensor/humid")
     public String getHumid() throws Exception{
-        return getReading("humid") + " %RH";
+        return "HUMIDITY: " + getReading("humid") + " %RH";
     }
 
     @RequestMapping("/sensor/test")
@@ -46,40 +46,10 @@ public class TempHumidityController {
     }
 
     @RequestMapping("/sensor/th")
-    public String getTempHumid() {
-        String s = null;
+    public String getTempHumid() throws Exception {
+        String reading = getReading("dht");
+        String[] arr = reading.split(" ");
 
-        System.out.println(":::::::::::::::::::::::::");
-        System.out.println(":::::::::::::::::::::::::");
-        try {
-
-            Process p = Runtime.getRuntime().exec("python3 PyScripts/dht2.py");
-
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(p.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(p.getErrorStream()));
-
-            // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
-            System.out.println(":::::::::::::::::::::::::");
-            System.out.println(":::::::::::::::::::::::::");
-        }
-        catch (IOException e) {
-            System.out.println("exception happened - here's what I know: ");
-            e.printStackTrace();
-        }
-
-        return s;
+        return "TEMP: " + arr[0] + "'C | HUMIDITY: " + arr[1] + " %RH";
     }
 }
