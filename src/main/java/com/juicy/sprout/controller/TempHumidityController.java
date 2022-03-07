0c2134b1fa03212565e97bdff8d8f5c3983eaf54
jay_s <jay_stefan@yahoo.com>
2022-03-06 23:29:05 -0500

    changed dir name

--- src/main/java/com/juicy/sprout/controller/TempHumidityController.java
+++ src/main/java/com/juicy/sprout/controller/TempHumidityController.java
@@ -42,29 +42,27 @@ public class TempHumidityController {
 
         try {
 
-            // run the Unix "ps -ef" command
-            // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec("python3 PythonScripts/dht2.py");
+            Process p = Runtime.getRuntime().exec("python3 PyScripts/test.py");
 
-//            BufferedReader stdInput = new BufferedReader(new
-//                    InputStreamReader(p.getInputStream()));
-//
-//            BufferedReader stdError = new BufferedReader(new
-//                    InputStreamReader(p.getErrorStream()));
-//
-//            // read the output from the command
-//            System.out.println("Here is the standard output of the command:\n");
-//            while ((s = stdInput.readLine()) != null) {
-//                System.out.println(s);
-//            }
-//
-//            // read any errors from the attempted command
-//            System.out.println("Here is the standard error of the command (if any):\n");
-//            while ((s = stdError.readLine()) != null) {
-//                System.out.println(s);
-//            }
-//
-//            System.exit(0);
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            System.exit(0);
         }
         catch (IOException e) {
             System.out.println("exception happened - here's what I know: ");
