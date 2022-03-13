39bde0ac0bc6b8c4b87cce691d0cb39d9cc59345
jay_s <jay_stefan@yahoo.com>
2022-03-12 19:05:15 -0500

    Initial webcam controller

---
+++ src/main/java/com/juicy/sprout/controller/WebcamController.java
@@ -0,0 +1,29 @@
+package com.juicy.sprout.controller;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+
+import javax.swing.*;
+
+public class WebcamController {
+
+    public static void main(String[] args) {
+        Webcam webcam = Webcam.getDefault();
+        webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+        WebcamPanel panel = new WebcamPanel(webcam);
+        panel.setFPSDisplayed(true);
+        panel.setDisplayDebugInfo(true);
+        panel.setImageSizeDisplayed(true);
+        panel.setMirrored(true);
+
+        JFrame window = new JFrame("Stream");
+        window.add(panel);
+        window.setResizable(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
