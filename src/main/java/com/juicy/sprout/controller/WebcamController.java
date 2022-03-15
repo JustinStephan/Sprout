d8b1bbae681a6410b10db8bb8368f5beaf3deaf0
jay_s <jay_stefan@yahoo.com>
2022-03-14 22:01:51 -0400

    Added opencv to pom and created a class to read Water Levels

--- src/main/java/com/juicy/sprout/controller/WebcamController.java
+++ src/main/java/com/juicy/sprout/controller/WebcamController.java
@@ -1,33 +1,40 @@
 package com.juicy.sprout.controller;
 
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.opencv.opencv_core.CvArr;
+import org.bytedeco.opencv.opencv_core.IplImage;
 
 import javax.swing.*;
+
+import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
 
 //@RestController
 public class WebcamController {
 
 //    @RequestMapping("/cam")
-    public static void main(String[] args) {
-        Webcam webcam = Webcam.getDefault();
-        webcam.setViewSize(WebcamResolution.VGA.getSize());
-
-        WebcamPanel panel = new WebcamPanel(webcam);
-        panel.setFPSDisplayed(true);
-        panel.setDisplayDebugInfo(true);
-        panel.setImageSizeDisplayed(true);
-        panel.setMirrored(true);
-
-        JFrame window = new JFrame("Stream");
-        window.add(panel);
-        window.setResizable(true);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
-    }
+//    public static void main(String[] args) {
+//        private static void captureFrame() {
+//            // 0-default camera, 1 - next...so on
+//            final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+//            try {
+//                grabber.start();
+//                Frame img = grabber.grab();
+//                CvArr arr = CvArr.
+//                if (img != null) {
+//                    cvSaveImage("capture.jpg", img);
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        JFrame window = new JFrame("Stream");
+//        window.add(panel);
+//        window.setResizable(true);
+//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        window.pack();
+//        window.setLocationRelativeTo(null);
+//        window.setVisible(true);
+//    }
 }
